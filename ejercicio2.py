a1ca7f219021608737fbb3aad3d018517e4c32c3
evamgarcia <emgarcia2911@gmail.com>
2022-09-26 21:49:29 +0200

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,2 +1,14 @@
-print("hola")
-print("hola")
+#Realiza un programa que cumpla el siguiente algoritmo utilizando siempre que sea posible operadores en asignación:
+#Guarda en una variable numero_magico el valor 12345679 (sin el 8)
+#Lee por pantalla otro numero_usuario, especifica que sea entre 1 y 9
+#Multiplica el numero_usuario por 9 en sí mismo
+#Multiplica el numero_magico por el numero_usuario en sí mismo
+#Finalmente muestra el valor final del numero_magico por pantalla
+
+from sys import float_repr_style
+
+
+numero_magico = 12345679
+numero_usuario = input(float("introduce un numero entre el 1 y el 9: "))
+numero_usuario2 = numero_usuario*9 
+print(numero_usuario2)
